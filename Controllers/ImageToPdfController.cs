82a5896a2616eb4ec3f9156831bcb1b23371ca4a
shirley <shirley.shen@wetax.com.cn>
2020-10-22 22:53:36 +0800

    返回pdf的bytes数组

--- Controllers/ImageToPdfController.cs
+++ Controllers/ImageToPdfController.cs
@@ -24,15 +24,15 @@ namespace ImageToPdfDemo.Controllers
         [HttpGet("test")]
         public string ImageToPdf()
         {
-            string imagePath = @"C:\Users\EDZ\Desktop\2.jpg";
-            string pdfPath = @"C:\Users\EDZ\Desktop\2.pdf";
+            string imagePath = @"D:\pics\1.jpeg";
+            string pdfPath = @"D:\pics\11.pdf";
             IImageFormat format = null;
             var imagesTemle = SixLabors.ImageSharp.Image.Load(imagePath, out format);
             using (var stream = new MemoryStream())
             {
                 imagesTemle.Save(stream, format);
                 stream.Flush();
-                BytesToPdf(stream.ToArray(), pdfPath);
+                BytesToPdf3(stream.ToArray(), pdfPath);
             }
 
             //ImageToPdf(imagePath, pdfPath);
@@ -95,52 +95,19 @@ namespace ImageToPdfDemo.Controllers
 
         private void BytesToPdf2(byte[] bytes, string pdfPath)
         {
-            var doc1 = new Document();
-            var stream2 = new MemoryStream(bytes, true);
-            using (stream2)
-            {
-                PdfWriter.GetInstance(doc1, stream2);
-                doc1.Open();
-
-                var image = iTextSharp.text.Image.GetInstance(bytes);
-                if (image.Height > PageSize.A4.Height)
-                {
-                    image.ScaleToFit(PageSize.A4.Width, PageSize.A4.Height);
-                }
-                else if (image.Width > PageSize.A4.Width)
-                {
-                    image.ScaleToFit(PageSize.A4.Width, PageSize.A4.Height);
-                }
-                image.Alignment = iTextSharp.text.Image.ALIGN_MIDDLE;
-                doc1.Add(image);
-
-                doc1.Close();
-            }
-           
-            stream2.ToArray();
-            return;
+            MemoryStream pdfStream = new MemoryStream();
             using (var doc = new PdfSharpCore.Pdf.PdfDocument())
             {
                 PdfSharpCore.Pdf.PdfPage page = doc.AddPage();//添加1个页面并获得该页面实例
-                var gfx = PdfSharpCore.Drawing.XGraphics.FromPdfPage(page);
 
-                var stream = new MemoryStream(bytes);
-                stream.Seek(0, SeekOrigin.Begin);
-
-                //PdfSharpCore.Drawing.XImage img = PdfSharpCore.Drawing.XImage.FromStream(stream);
-                //gfx.Draw
-                //page.Width = PdfSharpCore.Pdf. //注意pdf页面的单位是pt点，图片对象一般是px像素，所以要进行转换，大约1px=0.75pt
-                //page.Height = 图片对象.Height;
-                ////page.Size=PageSize.A4; //也可以直接设置为A4页面规格
-                //MemoryStream s = new MemoryStream(); //实例化内存Stream流，用于临时保存图片对象
-                //图片对象.Save(s, ImageFormat.Jpeg); //将图片对象保存到内存流中
-                //XImage img = XImage.FromStream(s);//从内存流中获取到图片对象，因为Image跟XImage不能直接变换，所以只能通过内存流做中转
-                //XGraphics g = XGraphics.FromPdfPage(page);//实例化XGraphics对象，并以刚才的pdf页面作为画布将后续的DrawImage、DrawString作用到画布上（画到画布上）
-                //g.DrawImage(img, 0, 0);//将图片画到画布上
-
-                doc.Save(stream, true);
-
+                page.Size = PdfSharpCore.PageSize.A4;
+                PdfSharpCore.Drawing.XImage img = PdfSharpCore.Drawing.XImage.FromStream(() => new MemoryStream(bytes));
+               
+                PdfSharpCore.Drawing.XGraphics g = PdfSharpCore.Drawing.XGraphics.FromPdfPage(page);//实例化XGraphics对象，并以刚才的pdf页面作为画布将后续的DrawImage、DrawString作用到画布上（画到画布上）
+                g.DrawImage(img, 0, 0);//将图片画到画布上
+                doc.Save(pdfPath);//将pdf文件保存到保存对话框获取到的路径上
             }
+
             //    var doc = new Document();
             //var stream = new FileStream(pdfPath, FileMode.Create, FileAccess.Write, FileShare.None);
             //using (stream)
@@ -161,6 +128,32 @@ namespace ImageToPdfDemo.Controllers
             //    doc.Add(image);
 
             //    doc.Close();
+        }
+
+
+        private void BytesToPdf3(byte[] bytes, string pdfPath)
+        {
+            var doc = new Document();
+            var stream = new MemoryStream();
+            using (stream)
+            {
+                PdfWriter.GetInstance(doc, stream);
+                doc.Open();
+
+                var image = iTextSharp.text.Image.GetInstance(bytes);
+                if (image.Height > PageSize.A4.Height)
+                {
+                    image.ScaleToFit(PageSize.A4.Width, PageSize.A4.Height);
+                }
+                else if (image.Width > PageSize.A4.Width)
+                {
+                    image.ScaleToFit(PageSize.A4.Width, PageSize.A4.Height);
+                }
+                image.Alignment = iTextSharp.text.Image.ALIGN_MIDDLE;
+                doc.Add(image);
+                doc.Close();
+            }
+            var result = stream.ToArray();
         }
     }
 }
